d77f2055f523e854cf85d996ccae17a9cd6461c4
Yaroslav Furman <yaro330@gmail.com>
2020-01-23 22:32:39 +0300

    video: backlight: silence debugging
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    (cherry picked from commit 305ec28cb3aa35a3c14257876b35e61c3fd8a865)

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -182,7 +182,6 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 			rc = -EINVAL;
 		else {
 			if ((!bd->use_count && brightness) || (bd->use_count && !brightness)) {
-				pr_info("%s: set brightness to %lu\n", __func__, brightness);
 				if (!bd->use_count)
 					bd->use_count++;
 				else
