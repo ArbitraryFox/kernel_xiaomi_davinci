bfbe9f7472789b748a6dc6299efc8d51c48831cb
Rahul Shahare <rshaha@codeaurora.org>
2020-05-13 03:25:03 +0530

    msm: ipa: update check flags to handle CONFIG_DEBUG_FS
    Add support to handle disabling of CONFIG_DEBUG_FS.
    
    Change-Id: I5164e0690e7e5165909ddfe759aa9694ad969f93
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/platform/msm/ipa/ipa_clients/ipa_mhi_client.c
+++ drivers/platform/msm/ipa/ipa_clients/ipa_mhi_client.c
@@ -260,11 +260,6 @@ fail_dma_enable:
 	return res;
 }
 
-#ifdef CONFIG_DEBUG_FS
-#define IPA_MHI_MAX_MSG_LEN 512
-static char dbg_buff[IPA_MHI_MAX_MSG_LEN];
-static struct dentry *dent;
-
 static char *ipa_mhi_channel_state_str[] = {
 	__stringify(IPA_HW_MHI_CHANNEL_STATE_DISABLE),
 	__stringify(IPA_HW_MHI_CHANNEL_STATE_ENABLE),
@@ -278,6 +273,11 @@ static char *ipa_mhi_channel_state_str[] = {
 	(((state) >= 0 && (state) <= IPA_HW_MHI_CHANNEL_STATE_ERROR) ? \
 	ipa_mhi_channel_state_str[(state)] : \
 	"INVALID")
+
+#ifdef CONFIG_DEBUG_FS
+#define IPA_MHI_MAX_MSG_LEN 512
+static char dbg_buff[IPA_MHI_MAX_MSG_LEN];
+static struct dentry *dent;
 
 static int ipa_mhi_print_channel_info(struct ipa_mhi_channel_ctx *channel,
 	char *buff, int len)
@@ -507,10 +507,12 @@ fail:
 	debugfs_remove_recursive(dent);
 }
 
+#ifdef CONFIG_DEBUG_FS
 static void ipa_mhi_debugfs_destroy(void)
 {
 	debugfs_remove_recursive(dent);
 }
+#endif
 #else
 static void ipa_mhi_debugfs_init(void) {}
 static void ipa_mhi_debugfs_destroy(void) {}
