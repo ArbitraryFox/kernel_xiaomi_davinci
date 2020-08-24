84c5c48ee9a8a9d9d84f68469ce31e7e75172bb8
ArbitraryFox <chrisung1992@gmail.com>
2020-08-24 14:17:30 +0800

    Revert "kernel: irq: manage: use a different way of affining perf IRQs"
    This reverts commit cee86a7067c0cdb32cdcb34af08858d2b35c2bad.

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1237,6 +1237,9 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 			if (ret)
 				goto out_thread;
 		}
+
+		if (new->flags & IRQF_PERF_CRITICAL)
+			affine_one_perf_thread(new->thread);
 	}
 
 	/*
